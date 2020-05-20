61be9e3b725ef4fd37e45213b58ae7106eb3a90d
Jose Peñuela Ruiz <jospearui@alum.us.es>
2020-05-20 20:08:15 +0200

    Nombres de vets ya no pueden tener signos de puntuacion

--- src/main/java/org/springframework/samples/petclinic/model/Person.java
+++ src/main/java/org/springframework/samples/petclinic/model/Person.java
@@ -19,6 +19,7 @@ package org.springframework.samples.petclinic.model;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 /**
  * Simple JavaBean domain object representing an person.
@@ -30,10 +31,12 @@ public class Person extends BaseEntity {
 
 	@Column(name = "first_name")
 	@NotEmpty
+	@Pattern(regexp = "[a-zA-Z]+", message = "no debe contener signos de puntuacion")
 	protected String	firstName;
 
 	@Column(name = "last_name")
 	@NotEmpty
+	@Pattern(regexp = "[a-zA-Z]+", message = "no debe contener signos de puntuacion")
 	protected String	lastName;
 
 
